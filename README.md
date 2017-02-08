175a0f2e45cdde4b11d97a08f3cb97bb9c396857
mk-pmb <mk-pmb@users.noreply.github.com>
2017-02-08 18:57:40 +0100

    Promote gist to repo so it gets an issue tracker

---
+++ README.md
@@ -0,0 +1,15 @@
+﻿
+Suppose a robot that can add sugar cubes to beverages.
+It has several methods for determining how many sugars to add,
+so there must be some code that determines the highest-priority answer.
+
+In [sugars.js](sugars.js) I collect the approaches I could think of,
+but they're all either rather bulky or won't work (as `||` won't stop
+at `0`), so at the bottom I suggest a new fallback operator that stops
+at the first defined (`!== undefined`) value, and one that stops at
+the first acceptable value as decided by a custom function.
+
+
+&nbsp;
+
+License: ISC
---
+++ sugars.js
@@ -0,0 +1,149 @@
+﻿/*jslint indent: 2, maxlen: 80, node: true */
+'use strict';
+
+// Suppose a robot that can add sugar cubes to beverages.
+// Let's mock some beverage scan result and config file data:
+
+var bev = { icon: '\u2615', color: 'sandybrown', opacity: 0.1, temp: 'hot' },
+  cfg = { defaultSugars: 0, sugarsByColor: { red: 2 } };
+
+// Methods for determining how many sugars to add, in order of priority:
+
+function guessFromColor(bev) { return cfg.sugarsByColor[bev.color]; }
+function queryHwdb() { return queryHwdb.errNotFound; }
+function surpriseMe() { return ((Math.random() * 42) + 0.2); }
+
+// Let's determine the highest-priority answer!
+
+
+console.log("The clean but bulky way:", (function ifReturnCopypasta() {
+
+  var n = guessFromColor(bev);
+  if (n !== undefined) { return n; }
+
+  n = queryHwdb(bev.idVendor, bev.idProduct);
+  if (n !== undefined) { return n; }
+
+  n = (cfg || false).defaultSugars;
+  if (n !== undefined) { return n; }
+
+  return surpriseMe(bev);
+
+}()));
+
+
+console.log("Can't just use ||:",
+  (guessFromColor(bev)
+    || queryHwdb(bev.idVendor, bev.idProduct)
+    || ((cfg || false).defaultSugars)
+    || surpriseMe(bev)
+  )
+  );
+
+
+console.log("Wrapper object via helper function:", (function () {
+
+  function wrapIfDefined(x) { return ((x !== undefined) && { wrapped: x }); }
+
+  return (wrapIfDefined(guessFromColor(bev))
+    || wrapIfDefined(queryHwdb(bev.idVendor, bev.idProduct))
+    || wrapIfDefined((cfg || false).defaultSugars)
+    || { wrapped: surpriseMe(bev) }).wrapped;
+
+}()));
+
+
+console.log("Cleaner looking wrapper function:", (function () {
+
+  function firstDefined() {
+    var idx, currentValue;
+    for (idx = 0; idx < arguments.length; idx += 1) {
+      currentValue = arguments[idx];
+      if (currentValue !== undefined) { break; }
+    }
+    return currentValue;
+  }
+
+  return firstDefined(
+    guessFromColor(bev),
+    queryHwdb(bev.idVendor, bev.idProduct),
+    (cfg || false).defaultSugars,
+    surpriseMe(bev)
+  );
+
+  // Problem: Always calculates all values, even if the first one is a match.
+}()));
+
+
+console.log("Use lots of functions:", (function () {
+
+  function firstDefining() {
+    var idx, currentValue;
+    for (idx = 0; idx < arguments.length; idx += 1) {
+      currentValue = arguments[idx]();
+      if (currentValue !== undefined) { break; }
+    }
+    return currentValue;
+  }
+
+  return firstDefining(
+    function () { return guessFromColor(bev); },
+    function () { return queryHwdb(bev.idVendor, bev.idProduct); },
+    function () { return (cfg || false).defaultSugars; },
+    function () { return surpriseMe(bev); }
+  );
+
+  // Problem: Always calculates all values, even if the first one is a match.
+}()));
+
+
+console.log("New syntax to the rescue!",
+  (guessFromColor(bev)
+    ?| queryHwdb(bev.idVendor, bev.idProduct)
+    ?| (cfg || false).defaultSugars
+    ?| surpriseMe(bev)
+  )
+  );
+
+
+// But what about libraries that prefer to return null?
+
+queryHwdb.errNotFound = null;
+function n2u(x) { return (x === null ? undefined : x); }
+
+console.log("Still cleaner than the wrapper objects:",
+  (guessFromColor(bev)
+    ?| n2u(queryHwdb(bev.idVendor, bev.idProduct))
+    ?| (cfg || false).defaultSugars
+    ?| surpriseMe(bev)
+  )
+  );
+
+
+// What if I totally need {undefined} in my set of acceptable values?
+
+function isAcceptableValue(x) {
+  if (x === undefined) { return true; }
+  if ((typeof x === 'number') && (x !== +x)) { return true; }
+  if (x === 23) { return true; }
+  return false;
+}
+
+console.log("Custom decider function: reminds of ternary operator",
+  ( ?| isAcceptableValue
+    : guessFromColor(bev)
+    : n2u(queryHwdb(bev.idVendor, bev.idProduct))
+    : (cfg || false).defaultSugars
+    : surpriseMe(bev)
+    // Beware: If there was no acceptable value, you still get the last one!
+    // So better append either a default or a last resort:
+    : function fail() { throw new Error('No acceptable value!'); }
+  )
+  );
+
+
+
+
+
+
+
